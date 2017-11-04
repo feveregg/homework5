51feec05a964538b74915a82eee541b5029819b6
feveregg <wk45@afsconnect2.njit.edu>
2017-11-04 11:55:11 -0400

    set information

--- index.php
+++ index.php
@@ -4,6 +4,15 @@
     ini_set('display_errors', 'On');
     error_reporting(E_ALL);
 
+    
 
+    $dsn = 'mysql:dbname=wk45;host=sql1.njit.edu';
+    $user = 'wk45';
+    $password = 'AmCb81UGG';
+
+
+
+    
+    
 
 ?>
