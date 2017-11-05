d97c7b404ba735cc838a13adb89ace18b60f9e13
feveregg <wk45@afsconnect2.njit.edu>
2017-11-05 14:41:30 -0500

    Set connection

--- index.php
+++ index.php
@@ -6,31 +6,27 @@
 
     
 
+    //Set Connection
     $dsn = 'mysql:dbname=wk45;host=sql1.njit.edu';
     $user = 'wk45';
     $password = 'AmCb81UGG';
 
 
-
     try {
 
-    	$dbh = new PDO($dsn, $user, $password);
-        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo 'Connected successfully<br>'
+    	$dbConn = new PDO($dsn, $user, $password);
+        $dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo 'Connected successfully<br>';
 
         } catch (PDOException $e) {
         
-            echo 'Connection failed: ' . $e->getMessage();
+            echo 'Connection failed: ' . $e->getMessage() . '<br>';
 
         }	
 
 
 
-    $sql = "SELECT * FROM accounts WHERE id<6";
 
-
-    
-    
 
 
 
